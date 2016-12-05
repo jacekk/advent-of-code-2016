d6c954c82874da480373cbd786ab2497ff95fbc5
jacekk <gitkraken@jacek-k.net>
2016-12-06 00:54:14 +0100

    day 5 - nice game chess - part 2;

--- day-05/chess.js
+++ day-05/chess.js
@@ -22,3 +22,54 @@ export const niceGameChess = (input) => {
 
 	return password
 }
+
+const BLANK_CHAR = '_'
+const POSITION_INDEX = 5
+const CHAR_INDEX = 6
+
+export const replaceAt = (haystack, index, character) => {
+	const indexAsInt = parseInt(index, 10)
+
+	return [
+		haystack.substring(0, indexAsInt),
+		character,
+		haystack.substring(indexAsInt + 1),
+	].join('')
+}
+
+const isPositionValid = (haystack, position) => {
+	const pos = parseInt(position, 10)
+
+	if (isNaN(pos)) {
+		return false
+	}
+
+	const charAtPos = haystack.substring(pos, pos + 1)
+
+	return pos >= 0 &&
+		pos < PASSWORD_LEN &&
+		charAtPos === BLANK_CHAR
+}
+
+export const niceGameChessBetter = (input) => {
+	let password = Array(PASSWORD_LEN).fill(BLANK_CHAR).join('')
+
+	for	(let i = 0; i < ITERATIONS_LIMIT; i += 1) {
+		const hash = md5(input + i)
+
+		if (hash.indexOf(PREFIX) === 0) {
+			const pos = hash.substring(POSITION_INDEX, POSITION_INDEX + 1)
+			const char = hash.substring(CHAR_INDEX, CHAR_INDEX + 1)
+
+			if (isPositionValid(password, pos)) {
+				password = replaceAt(password, pos, char)
+			}
+		}
+
+		if (password.indexOf(BLANK_CHAR) < 0) {
+			break
+		}
+	}
+
+	return password
+}
--- day-05/chess.test.js
+++ day-05/chess.test.js
@@ -1,7 +1,15 @@
-import { niceGameChess } from './chess'
+import { niceGameChess, niceGameChessBetter, replaceAt } from './chess'
 
 describe('day 5 - chess', () => {
-	xit('should find the password from door id', () => {
+	it('should replace string chart at given position', () => {
+		expect(replaceAt('abc', 0, 'z')).toEqual('zbc')
+		expect(replaceAt('___', 1, 'a')).toEqual('_a_')
+		expect(replaceAt('________', 5, 5)).toEqual('_____5__')
+		expect(replaceAt('___', '1', 5)).toEqual('_5_')
+	})
+
+	xit('should find the password from door id', () => { // skipped; takes lot of minutes to execute
 		expect(niceGameChess('abc')).toEqual('18f47a30')
+		expect(niceGameChessBetter('abc')).toEqual('05ace8e3')
 	})
 })
