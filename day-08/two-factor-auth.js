cf8f08e496fc9b7995ca6ed6b00bc45ce9fe82a9
jacekk <github.com@jacek-k.net>
2016-12-08 21:15:19 +0100

    day 8 - two-factor auth;

---
+++ day-08/two-factor-auth.js
@@ -0,0 +1,110 @@
+import extend from 'extend'
+
+
+export const PX_ON = '#'
+export const PX_OFF = '.'
+export const SIZE_HEI = 6
+export const SIZE_WID = 50
+
+const int = input => parseInt(input, 10)
+const flatten = nested => [].concat([], ...nested)
+
+/*
+export const printScreen = (screen) => {
+	const stdout = screen
+		.map(row => row.join(''))
+		.join('\n')
+
+	console.log(stdout)
+}
+*/
+
+export const makeEmptyScreen = () => Array(SIZE_HEI)
+	.fill()
+	.map(() => Array(SIZE_WID).fill(PX_OFF))
+
+export const rect = (screen, wid, hei) => {
+	const modified = extend(true, [], screen)
+
+	for (let rowIndex = 0; rowIndex < hei && rowIndex < SIZE_HEI; rowIndex += 1) {
+		for (let colIndex = 0; colIndex < wid && rowIndex < SIZE_WID; colIndex += 1) {
+			modified[rowIndex][colIndex] = PX_ON
+		}
+	}
+
+	return modified
+}
+
+export const shiftValues = (values, offset) => {
+	const endValues = values.splice(-offset)
+
+	Array.prototype.unshift.apply(values, endValues)
+
+	return values
+}
+
+export const rotateRow = (screen, index, offset) => {
+	const modified = extend(true, [], screen)
+	let rowValues = screen[index]
+	rowValues = shiftValues(rowValues, offset)
+	modified[index] = rowValues
+
+	return modified
+}
+
+export const rotateColumn = (screen, index, offset) => {
+	const getColValues = row => row
+		.filter((col, colIndex) => colIndex === index)
+		.pop()
+
+	const modified = extend(true, [], screen)
+	let colValues = screen.map(getColValues)
+	colValues = shiftValues(colValues, offset)
+
+	colValues.forEach((item, rowIndex) => {
+		modified[rowIndex][index] = item
+	})
+
+	return modified
+}
+
+
+export const operate = (screen, operation) => {
+	const REG_EXP_RECT = /rect (\d+)x(\d+)/
+	const REG_EXP_ROTATE_ROW = /rotate row y=(\d+) by (\d+)/
+	const REG_EXP_ROTATE_COL = /rotate column x=(\d+) by (\d+)/
+
+	const rectMatch = operation.match(REG_EXP_RECT)
+	const rotRowMatch = operation.match(REG_EXP_ROTATE_ROW)
+	const rotColMatch = operation.match(REG_EXP_ROTATE_COL)
+
+	if (rectMatch) {
+		return rect(screen, int(rectMatch[1]), int(rectMatch[2]))
+	}
+	if (rotRowMatch) {
+		return rotateRow(screen, int(rotRowMatch[1]), int(rotRowMatch[2]))
+	}
+	if (rotColMatch) {
+		return rotateColumn(screen, int(rotColMatch[1]), int(rotColMatch[2]))
+	}
+
+	return screen
+}
+
+export const multipleOperations = (screen, input) => {
+	const lines = input.trim().split('\n')
+	let modified = extend(true, [], screen)
+
+	lines
+		.map(line => line.trim())
+		.filter(Boolean)
+		.forEach((line) => {
+			modified = operate(modified, line)
+		})
+
+	return modified
+}
+
+export const countOn = screen => flatten(screen)
+	.filter(px => px === PX_ON)
+	.length
---
+++ day-08/two-factor-auth.test.js
@@ -0,0 +1,150 @@
+import { PX_ON, PX_OFF, makeEmptyScreen, rect, rotateColumn, rotateRow, operate, multipleOperations, countOn } from './two-factor-auth'
+
+describe('day 8 - two factor auth', () => {
+	it('should create empty scrren', () => {
+		const screen = makeEmptyScreen()
+
+		expect(screen.length).toEqual(6)
+		expect(screen[0].length).toEqual(50)
+		expect(screen[0][0]).toEqual('.')
+		expect(screen.pop().pop()).toEqual('.')
+	})
+
+	it('should light on rectangular of specific size', () => {
+		let screen = makeEmptyScreen()
+		screen = rect(screen, 2, 2)
+
+		expect(screen[0][0]).toEqual(PX_ON)
+		expect(screen[0][1]).toEqual(PX_ON)
+		expect(screen[1][0]).toEqual(PX_ON)
+		expect(screen[1][1]).toEqual(PX_ON)
+
+		expect(screen[2][2]).toEqual(PX_OFF)
+	})
+
+	it('should rotate screen column by some pixels', () => {
+		let screen = makeEmptyScreen()
+		screen = rect(screen, 3, 3)
+		screen = rotateColumn(screen, 1, 2)
+
+		expect(screen[0][1]).toEqual(PX_OFF)
+		expect(screen[1][1]).toEqual(PX_OFF)
+		expect(screen[2][1]).toEqual(PX_ON)
+		expect(screen[3][1]).toEqual(PX_ON)
+		expect(screen[4][1]).toEqual(PX_ON)
+		expect(screen[5][1]).toEqual(PX_OFF)
+	})
+
+	it('should rotate screen row by some pixels', () => {
+		let screen = makeEmptyScreen()
+		screen = rect(screen, 3, 3)
+		screen = rotateRow(screen, 1, 4)
+
+		expect(screen[0][0]).toEqual(PX_ON)
+		expect(screen[0][1]).toEqual(PX_ON)
+		expect(screen[0][2]).toEqual(PX_ON)
+		expect(screen[0][3]).toEqual(PX_OFF)
+
+		expect(screen[1][1]).toEqual(PX_OFF)
+		expect(screen[1][2]).toEqual(PX_OFF)
+		expect(screen[1][3]).toEqual(PX_OFF)
+		expect(screen[1][4]).toEqual(PX_ON)
+		expect(screen[1][5]).toEqual(PX_ON)
+		expect(screen[1][6]).toEqual(PX_ON)
+		expect(screen[1][7]).toEqual(PX_OFF)
+	})
+
+	it('should detect rect operation', () => {
+		let screen = makeEmptyScreen()
+
+		expect(screen[0][0]).toEqual(PX_OFF)
+
+		screen = operate(screen, 'rect 2x3')
+
+		expect(screen[0][0]).toEqual(PX_ON)
+		expect(screen[0][1]).toEqual(PX_ON)
+		expect(screen[0][2]).toEqual(PX_OFF)
+
+		expect(screen[1][0]).toEqual(PX_ON)
+		expect(screen[1][1]).toEqual(PX_ON)
+		expect(screen[1][2]).toEqual(PX_OFF)
+
+		expect(screen[2][0]).toEqual(PX_ON)
+		expect(screen[2][1]).toEqual(PX_ON)
+		expect(screen[2][2]).toEqual(PX_OFF)
+
+		expect(screen[3][0]).toEqual(PX_OFF)
+		expect(screen[3][1]).toEqual(PX_OFF)
+		expect(screen[3][2]).toEqual(PX_OFF)
+	})
+
+	it('should detect rotate row operation', () => {
+		let screen = makeEmptyScreen()
+
+		screen = rect(screen, 3, 3)
+		screen = operate(screen, 'rotate row y=1 by 2')
+
+		expect(screen[0][0]).toEqual(PX_ON)
+		expect(screen[0][1]).toEqual(PX_ON)
+		expect(screen[0][2]).toEqual(PX_ON)
+		expect(screen[0][3]).toEqual(PX_OFF)
+
+		expect(screen[1][0]).toEqual(PX_OFF)
+		expect(screen[1][1]).toEqual(PX_OFF)
+		expect(screen[1][2]).toEqual(PX_ON)
+		expect(screen[1][3]).toEqual(PX_ON)
+		expect(screen[1][4]).toEqual(PX_ON)
+		expect(screen[1][5]).toEqual(PX_OFF)
+
+		expect(screen[2][0]).toEqual(PX_ON)
+		expect(screen[2][1]).toEqual(PX_ON)
+		expect(screen[2][2]).toEqual(PX_ON)
+		expect(screen[2][3]).toEqual(PX_OFF)
+	})
+
+	it('should detect rotate column operation', () => {
+		let screen = makeEmptyScreen()
+
+		screen = rect(screen, 3, 3)
+		screen = operate(screen, 'rotate column x=2 by 3')
+
+		expect(screen[0][2]).toEqual(PX_OFF)
+		expect(screen[1][2]).toEqual(PX_OFF)
+		expect(screen[2][2]).toEqual(PX_OFF)
+		expect(screen[3][2]).toEqual(PX_ON)
+		expect(screen[4][2]).toEqual(PX_ON)
+		expect(screen[5][2]).toEqual(PX_ON)
+	})
+
+	it('should not modify screen if no operation has been detected', () => {
+		const src = makeEmptyScreen()
+		const modified = operate(src, '')
+
+		expect(src).toEqual(modified)
+	})
+
+	it('should parse list of operations', () => {
+		let screen = makeEmptyScreen()
+
+		screen = multipleOperations(screen, `
+			rect 3x3
+			rotate column x=2 by 1
+			rotate row y=2 by 2
+			rect 3x3
+		`)
+
+		expect(screen[0][2]).toEqual(PX_ON)
+		expect(screen[2][4]).toEqual(PX_ON)
+		expect(screen[2][3]).toEqual(PX_ON)
+	})
+
+	it('should cound pixels that are on', () => {
+		expect(countOn(rect(makeEmptyScreen(), 1, 1))).toEqual(1)
+		expect(countOn(rect(makeEmptyScreen(), 2, 2))).toEqual(4)
+		expect(countOn(rect(makeEmptyScreen(), 10, 5))).toEqual(50)
+
+		const screen = rect(makeEmptyScreen(), 10, 5)
+		screen[0][0] = PX_OFF
+		expect(countOn(screen)).toEqual(49)
+	})
+})
